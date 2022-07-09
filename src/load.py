7b67344bd33c8aa4cd5fdde16321156c7b77f47c
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-09 19:45:49 -0400

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,3 +1,6 @@
 from namedb import get
+from .search import search
+from .set import set
 def load(user_id: int):
+  if search(user_id) is False: set(user_id, 'en')
   return get(user_id, categorie="language")
